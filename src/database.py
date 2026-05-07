79212c9f9a70fa6fad81d2c85323191d624bb5d9
Paweł Niewierowski <99548252+nitrazek@users.noreply.github.com>
2026-05-07 20:24:11 +0200

    Added plugins support for db

--- src/database.py
+++ src/database.py
@@ -1,14 +1,21 @@
 import os
+from sqlalchemy import text
 from sqlmodel import create_engine, Session, SQLModel
 
 class DatabaseManager:
     DB_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:postgres@localhost:5432/postgres")
     
-    engine = create_engine(DB_URL, echo=False)
+    engine = create_engine(DB_URL, echo=False, pool_pre_ping=True)
 
     @classmethod
     def create_db_and_tables(cls):
+        with cls.get_session() as session:
+            session.exec(text("CREATE EXTENSION IF NOT EXISTS postgis"))
+            session.exec(text("CREATE EXTENSION IF NOT EXISTS pgrouting"))
+            session.commit()
+        
         SQLModel.metadata.create_all(cls.engine)
+        print("Database initialized: Extensions and tables are ready.")
 
     @classmethod
     def get_session(cls):
