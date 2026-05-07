caf75ec3f0979cb56831348c37e1dbedcaea2f80
Paweł Niewierowski <99548252+nitrazek@users.noreply.github.com>
2026-05-07 20:12:33 +0200

    Created connection to db

--- src/database.py
+++ src/database.py
@@ -1,1 +1,15 @@
-print("Bluetooth connected successfully")
+import os
+from sqlmodel import create_engine, Session, SQLModel
+
+class DatabaseManager:
+    DB_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:postgres@localhost:5432/postgres")
+    
+    engine = create_engine(DB_URL, echo=False)
+
+    @classmethod
+    def create_db_and_tables(cls):
+        SQLModel.metadata.create_all(cls.engine)
+
+    @classmethod
+    def get_session(cls):
+        return Session(cls.engine)
