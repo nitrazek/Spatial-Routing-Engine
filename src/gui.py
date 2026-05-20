a4277211d46c9c511e246a37df90fc5f194a3974
sh4dqwx <bart.karolczak@gmail.com>
2026-05-20 20:11:48 +0200

    added calculating route for PR

--- src/gui.py
+++ src/gui.py
@@ -68,6 +68,32 @@ class RoutingApp:
         btn_calc = tk.Button(self.control_frame, text="Wyznacz trasę", bg="#4CAF50", fg="white", font=("Arial", 12, "bold"), command=self.calculate_and_draw)
         btn_calc.pack(fill="x", pady=(30, 0))
 
+    def _draw_road_route(self, shortest_route: routing.ShortestRoute):
+        color = self.mode_colors.get(enums.RouteMode.CAR, "#000000")
+        self.map_widget.set_path(
+            position_list=shortest_route.nodes,
+            color=color,
+            width=4
+        )
+
+    def _draw_transit_route(self, shortest_route: routing.ShortestRoute):
+        color = self.mode_colors.get(enums.RouteMode.PUBLIC, "#000000")
+        self.map_widget.set_path(
+            position_list=shortest_route.nodes,
+            color=color,
+            width=4
+        )
+
+    def _draw_pr_route(self, shortest_route: routing.PrShortestRoute):
+        color = self.mode_colors.get(enums.RouteMode.PR, "#000000")
+        self.map_widget.set_path(
+            position_list=shortest_route.nodes,
+            color=color,
+            width=4
+        )
+        self.map_widget.set_marker(shortest_route.pr_node[0], shortest_route.pr_node[1], text="PR", marker_color_circle="darkgray", marker_color_outside="black")
+
+
     def calculate_and_draw(self):
         """Pobiera dane, wywołuje 'algorytm' i rysuje trasę na mapie."""
         try:
@@ -88,27 +114,20 @@ class RoutingApp:
         self.map_widget.set_marker(source[0], source[1], text="START", marker_color_circle="green", marker_color_outside="darkgreen")
         self.map_widget.set_marker(target[0], target[1], text="CEL", marker_color_circle="red", marker_color_outside="darkred")
 
-        routing_fn_map = {
-            enums.RouteMode.CAR: routing.calculate_shortest_route_road,
-            enums.RouteMode.PUBLIC: routing.calculate_shortest_route_transit,
-            enums.RouteMode.PR: routing.calculate_shortest_route_pr,
+        fn_map = {
+            enums.RouteMode.CAR: {"route": routing.calculate_shortest_route_road, "draw": self._draw_road_route},
+            enums.RouteMode.PUBLIC: {"route": routing.calculate_shortest_route_transit, "draw": self._draw_transit_route},
+            enums.RouteMode.PR: {"route": routing.calculate_shortest_route_pr, "draw": self._draw_pr_route},
         }
 
         for mode in selected_modes:
-            # --- MOCK DANYCH (Twój kod testowy) ---
-            nodes, cost = routing_fn_map[mode]()
-            
-            color = self.mode_colors.get(mode, "#000000")
-            self.map_widget.set_path(
-                position_list=nodes,
-                color=color,
-                width=4
-            )
+            shortest_route: routing.ShortestRoute = fn_map[mode]["route"](source=source, target=target)
+            fn_map[mode]["draw"](shortest_route=shortest_route)
 
-        min_lat = min(node[0] for node in nodes)
-        max_lat = max(node[0] for node in nodes)
-        min_lon = min(node[1] for node in nodes)
-        max_lon = max(node[1] for node in nodes)
+        min_lat = min(node[0] for node in shortest_route.nodes)
+        max_lat = max(node[0] for node in shortest_route.nodes)
+        min_lon = min(node[1] for node in shortest_route.nodes)
+        max_lon = max(node[1] for node in shortest_route.nodes)
         self.map_widget.fit_bounding_box((max_lat, min_lon), (min_lat, max_lon))
 
 if __name__ == "__main__":
--- src/routing.py
+++ src/routing.py
@@ -13,14 +13,31 @@ class ShortestRoute:
     cost: float
 
 
-def get_nearest_node(x: float, y: float, engine: Engine, table_name: str) -> int:
+@dataclass
+class PrShortestRoute(ShortestRoute):
+    pr_node: tuple[float, float]
+
+
+def get_nearest_node(x: float, y: float, engine: Engine, nodes_table_name: str) -> int:
     query = text(f"""
-        SELECT id FROM {table_name}
+        SELECT id FROM {nodes_table_name}
         ORDER BY geom <-> ST_SetSRID(ST_MakePoint(:x, :y), 4326)
         LIMIT 1;
     """)
     with engine.connect() as conn:
         return conn.execute(query, {"x": x, "y": y}).scalar()
+
+
+def run_dijkstra(source: int, target: int, engine: Engine, edges_table_name: str, nodes_table_name: str) -> pd.DataFrame:
+    return pd.read_sql_query(f"""
+        SELECT n.x, n.y, d.agg_cost
+        FROM pgr_Dijkstra(
+            'SELECT id, source, target, cost, reverse_cost FROM {edges_table_name}',
+            {source}, {target},
+            directed => false
+        ) d
+        JOIN {nodes_table_name} n ON d.node = n.id;
+    """, engine)
 
 
 def calculate_shortest_route_road():
@@ -35,32 +52,45 @@ def calculate_shortest_route_pr(source: tuple[float, float], target: tuple[float
     engine = database.DatabaseManager.engine
 
     source_id = get_nearest_node(
-        x=source[0],
-        y=source[1],
+        x=source[1],
+        y=source[0],
         engine=engine,
-        table_name="road_nodes"
+        nodes_table_name="road_nodes"
     )
-
     target_id = get_nearest_node(
-        x=target[0],
-        y=target[1],
+        x=target[1],
+        y=target[0],
         engine=engine,
-        table_name="transit_nodes"
+        nodes_table_name="transit_nodes"
     )
 
     pr_nodes = pd.read_sql_query("""
-        SELECT id, road_node, transit_node 
+        SELECT id, x, y, road_node, transit_node 
         FROM pr_nodes;
     """, engine)
 
+    routes: list[ShortestRoute] = []
     for _, pr in pr_nodes.iterrows():
-        rode_route = f"""
-            SELECT *
-            FROM pgr_Dijkstra(
-                'SELECT id, source, target, cost, reverse_cost FROM transit_edges',
-                {source_id}, {pr['road_node']},
-                directed => false
-            );
-        """
+        road_route_df = run_dijkstra(
+            source=source_id,
+            target=pr['road_node'],
+            engine=engine,
+            edges_table_name="road_edges",
+            nodes_table_name="road_nodes"
+        )
+        transit_route_df = run_dijkstra(
+            source=pr['transit_node'],
+            target=target_id,
+            engine=engine,
+            edges_table_name="transit_edges",
+            nodes_table_name="transit_nodes"
+        )
 
-    
+        if road_route_df.empty or transit_route_df.empty:
+            continue
+
+        nodes = list(zip(road_route_df['y'], road_route_df['x'])) + list(zip(transit_route_df['y'], transit_route_df['x']))
+        cost = road_route_df['agg_cost'].iloc[-1] + transit_route_df['agg_cost'].iloc[-1]
+        routes.append(PrShortestRoute(nodes=nodes, cost=cost, pr_node=(pr['y'], pr['x'])))
+
+    return min(routes, key=lambda x: x.cost)
